26c214327e0e9ff419eca911304dd4e2e9ccb2f3
courtneymickelsen <lam15020@byui.edu>
2022-10-06 15:05:54 -0600

    swagger documentation up and running

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const app = express();
 const mongodb = require('./db/connect');
 const bodyParser = require('body-parser');
-const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 const port = process.env.PORT;
 
-app.use(bodyParser.json())
-.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  next();
-})
-.use('/', require('./routes'));
+app
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
 
 mongodb.initDb((err) => {
     if(err){
---
+++ swagger.js
@@ -0,0 +1,15 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+    info: {
+        title: "Courtney's API",
+        description: "Courtney's API for Backend II"
+    },
+    host: 'web-backend-ii.onrender.com',
+    schemes: ['http'],
+};
+
+const outputFile = './swagger.json';
+const endpointFiles = ['./server.js'];
+
+swaggerAutogen(outputFile, endpointFiles, doc);
