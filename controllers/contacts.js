acc1228af32a34fd548748ade2a2e40d24f8a510
courtneymickelsen <lam15020@byui.edu>
2022-10-08 22:30:52 -0600

    anddd more for errors.

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -23,49 +23,61 @@ const getSingle = async (req, res) => {
 };
 
 const createContact = async (req, res) => {
-  const contactObj = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    favoriteColor: req.body.favoriteColor,
-    birthday: req.body.birthday
-  };
-  const response = await mongodb.getDb().db('courtneyDatabase').collection('contacts').insertOne(contactObj);
-
-  if (response.acknowledged) {
-    res.status(201).json(response);
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+  try {
+    const contactObj = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+    const response = await mongodb.getDb().db('courtneyDatabase').collection('contacts').insertOne(contactObj);
+  
+    if (response.acknowledged) {
+      res.status(201).json(response);
+    } else {
+      res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+    }
+  } catch(err) {
+    res.status(500).json(err);
   }
 }
 
 const updateContact = async (req, res) => {
-  const contactObj = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    favoriteColor: req.body.favoriteColor,
-    birthday: req.body.birthday
-  };
-
-  const userId = new ObjectId(req.params.id);
-  const response = await mongodb.getDb().db('courtneyDatabase').collection('contacts').replaceOne({_id: userId}, contactObj);
-
-  if (response.modifiedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).send(response.error);
+  try {
+    const contactObj = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+  
+    const userId = new ObjectId(req.params.id);
+    const response = await mongodb.getDb().db('courtneyDatabase').collection('contacts').replaceOne({_id: userId}, contactObj);
+  
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).send(response.error);
+    }
+  } catch(err) {
+    res.status(500).json(err);
   }
 }
 
 const deleteContact = async (req, res) => {
+  try {
   const userId = new ObjectId(req.params.id);
-  const response = await mongodb.getDb().db('courtneyDatabase').collection('contacts').deleteOne({_id: userId});
-
-  if (response.deletedCount > 0){
-    res.status(200).send();
-  } else {
-    res.status(500).send(response.error);
+    const response = await mongodb.getDb().db('courtneyDatabase').collection('contacts').deleteOne({_id: userId});
+  
+    if (response.deletedCount > 0){
+      res.status(200).send();
+    } else {
+      res.status(500).send(response.error);
+    }
+  } catch(err) {
+    res.status(500).json(err);
   }
 }
 
